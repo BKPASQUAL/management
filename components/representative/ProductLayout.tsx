6964835da51c73c8c64d8cd6de0ab9fdfd673cba
Bawantha <bawanthapasqual@gmail.com>
2025-08-20 19:36:24 +0530

    grid layaout chnage

--- components/representative/ProductLayout.tsx
+++ components/representative/ProductLayout.tsx
@@ -78,7 +78,7 @@ const products = [
 export default function ProductLayout() {
   return (
     <div className="">
-      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-7 gap-2 sm:gap-4">
+      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-7 gap-2 sm:gap-4">
         {products.map((product) => (
           <div
             key={product.id}
