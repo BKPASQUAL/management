ece9cebfafdd351d0169cc991fcd0838a8332287
Bawantha <bawanthapasqual@gmail.com>
2025-10-03 12:00:03 +0530

    store update

--- store/store.ts
+++ store/store.ts
@@ -3,32 +3,36 @@ import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { api } from "./services/api";
 import { authApi } from "./services/auth";
+import { supplierApi } from "./services/supplier";
+import { productApi } from "./services/product";
+import { stockApi } from "./services/stock";
 import { customerApi } from "./services/customer";
 import { customerBillApi } from "./services/customerBill";
-import { stockApi } from "./services/stock";
 import authReducer from "./slices/authSlice";
-// import cartReducer from "./slices/cartSlice";
 
 export const store = configureStore({
   reducer: {
-    // Add all your API reducers
+    // API reducers
     [api.reducerPath]: api.reducer,
     [authApi.reducerPath]: authApi.reducer,
+    [supplierApi.reducerPath]: supplierApi.reducer,
+    [productApi.reducerPath]: productApi.reducer,
+    [stockApi.reducerPath]: stockApi.reducer,
     [customerApi.reducerPath]: customerApi.reducer,
     [customerBillApi.reducerPath]: customerBillApi.reducer,
-    [stockApi.reducerPath]: stockApi.reducer,
 
     // Regular slices
     auth: authReducer,
-    // cart: cartReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(
       api.middleware,
       authApi.middleware,
+      supplierApi.middleware,
+      productApi.middleware,
+      stockApi.middleware,
       customerApi.middleware,
-      customerBillApi.middleware,
-      stockApi.middleware
+      customerBillApi.middleware
     ),
 });
 
